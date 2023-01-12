b39a64e6d5b1287348c6b9a06d72b65453286cb4
park <s10e01s@naver.com>
2023-01-13 01:12:14 +0900

    3.회원 가입 컨트롤러

---
+++ src/main/java/com/studyolle/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.studyolle.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token",
+                        "/email-login", "/check-email-login", "/login-link").permitAll()
+                .requestMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
