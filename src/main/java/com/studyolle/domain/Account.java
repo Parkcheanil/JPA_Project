a3be791d5cd69be41c88a2a2f1a33c1f31cf6400
박천일 <63269916+Parkchenil@users.noreply.github.com>
2023-03-12 22:52:43 +0900

    38.관심 주제 테스트

--- src/main/java/com/studyolle/domain/Account.java
+++ src/main/java/com/studyolle/domain/Account.java
@@ -6,6 +6,7 @@ import lombok.*;
 import org.hibernate.sql.exec.spi.StandardEntityInstanceResolver;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
@@ -57,7 +58,7 @@ public class Account {
     private boolean studyUpdatedByWeb = true;
 
     @ManyToMany
-    private Set<Tag> tags;
+    private Set<Tag> tags = new HashSet<>();
 
     public void generateEmailCheckToken() {
         this.emailCheckToken = UUID.randomUUID().toString();
--- src/test/java/com/studyolle/settings/SettingsControllerTest.java
+++ src/test/java/com/studyolle/settings/SettingsControllerTest.java
@@ -1,17 +1,23 @@
 package com.studyolle.settings;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.studyolle.WithAccount;
 import com.studyolle.account.AccountRepository;
 import com.studyolle.account.AccountService;
 import com.studyolle.domain.Account;
+import com.studyolle.domain.Tag;
+import com.studyolle.settings.form.TagForm;
+import com.studyolle.tag.TagRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -19,6 +25,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 class SettingsControllerTest {
@@ -26,10 +33,64 @@ class SettingsControllerTest {
     @Autowired MockMvc mockMvc;
     @Autowired AccountRepository accountRepository;
     @Autowired PasswordEncoder passwordEncoder;
-
+    @Autowired ObjectMapper objectMapper;
+    @Autowired TagRepository tagRepository;
+    @Autowired AccountService accountService;
     @AfterEach
     void afterEach() {
         accountRepository.deleteAll();
+    }
+
+    @WithAccount("park")
+    @DisplayName("계정의 태그 수정 폼")
+    @Test
+    void updateTagsForm() throws Exception {
+        mockMvc.perform(get(SettingsController.SETTINGS_TAGS_URL))
+                .andExpect(view().name(SettingsController.SETTINGS_TAGS_VIEW_NAME))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("whitelist"))
+                .andExpect(model().attributeExists("tags"));
+    }
+
+    @WithAccount("park")
+    @DisplayName("계정에 태그 추가")
+    @Test
+    void addTag() throws Exception {
+        TagForm tagForm = new TagForm();
+        tagForm.setTagTitle("newTag");
+
+        mockMvc.perform(post(SettingsController.SETTINGS_TAGS_URL + "/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(tagForm))
+                .with(csrf()))
+                .andExpect(status().isOk());
+
+        Tag newTag = tagRepository.findByTitle("newTag");
+        assertNotNull(newTag);
+        Account park = accountRepository.findByNickname("park");
+        assertTrue(park.getTags().contains(newTag));
+    }
+
+    @WithAccount("park")
+    @DisplayName("계정에 태그 삭제")
+    @Test
+    void removeTag() throws Exception {
+        Account park = accountRepository.findByNickname("park1");
+        Tag newTag = tagRepository.save(Tag.builder().title("newTag").build());
+        accountService.addTag(park, newTag);
+
+        assertTrue(park.getTags().contains(newTag));
+
+        TagForm tagForm = new TagForm();
+        tagForm.setTagTitle("newTag");
+
+        mockMvc.perform(post(SettingsController.SETTINGS_TAGS_URL + "/remove")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tagForm))
+                        .with(csrf()))
+                .andExpect(status().isOk());
+
+        assertFalse(park.getTags().contains(newTag));
     }
 
     @WithAccount("park")
@@ -132,7 +193,7 @@ class SettingsControllerTest {
     @WithAccount("park")
     @DisplayName("닉네임 수정하기 - 입력값 정상")
     @Test
-    void updateNickname() throws Exception {
+    void updateNickname_success() throws Exception {
         String newNickname = "park1";
         mockMvc.perform(post(SettingsController.SETTINGS_ACCOUNT_URL)
                         .param("nickname", newNickname)
@@ -147,16 +208,15 @@ class SettingsControllerTest {
     @WithAccount("park")
     @DisplayName("닉네임 수정하기 - 입력값 에러")
     @Test
-    void updateNickname_error() throws Exception {
-        String newNickname = "-\\_()_/-";
+    void updateNickname_failure() throws Exception {
+        String newNickname = "-\\_()_//-";
         mockMvc.perform(post(SettingsController.SETTINGS_ACCOUNT_URL)
-                        .param("nickname", newNickname)
-                        .with(csrf()))
+                .param("nickname", newNickname)
+                .with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(view().name(SettingsController.SETTINGS_ACCOUNT_VIEW_NAME))
+                .andExpect(model().hasErrors())
                 .andExpect(model().attributeExists("account"))
-                .andExpect(model().attributeExists("nicknameForm"))
-                .andExpect(model().hasErrors());
-
+                .andExpect(model().attributeExists("nicknameForm"));
     }
 }
